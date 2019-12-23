25fff2f7c6a0016f914b57833fb8c2c0d77decf8
aineshp <56926374+aineshp@users.noreply.github.com>
2019-12-23 18:45:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var canvas;
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
+var allPlayers;
+
+
+function setup(){
+    database=firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    game=new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    background("white");
+if(playerCount===4){
+    game.update(1);
+
+}
+if(gameState===1){
+    clear();
+    game.play();
+    
+}
+   
+
+}
+
+
